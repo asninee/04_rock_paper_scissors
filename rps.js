7d0fce10b430407a24b9e66a9a6a147d8f1e3ab5
nine96as <nine96@skiff.com>
2023-10-27 15:40:01 +0100

    Beautify outputs

--- rps.js
+++ rps.js
@@ -2,14 +2,14 @@ const rps = (input1, input2) => {
   return (input1 === 'R' && input2 === 'S') ||
     (input1 === 'S' && input2 === 'P') ||
     (input1 === 'P' && input2 === 'R')
-    ? 'Player 1 wins!'
+    ? `P1: ${input1}\nP2: ${input2}\n=> Player 1 wins!`
     : (input2 === 'R' && input1 === 'S') ||
       (input2 === 'S' && input1 === 'P') ||
       (input2 === 'P' && input1 === 'R')
-    ? 'Player 2 wins!'
-    : 'Draw!';
+    ? `P1: ${input1}\nP2: ${input2}\n=> Player 2 wins!`
+    : `P1: ${input1}\nP2: ${input2}\n=> It's a draw!`;
 };
 
-modules.export = {
+module.exports = {
   rps
 };
