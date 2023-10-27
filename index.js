5cc4fafb63e0b3e2f48e6b88c5c0d74f10e97fdf
nine96as <nine96@skiff.com>
2023-10-27 16:26:22 +0100

    Get colors working properly

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const prompt = require('prompt-sync')({ sigint: true });
 const { rps } = require('./rps');
+const c = require('ansi-colors');
 
 let carryOn = true;
-const validChars = ['R', 'P', 'C'];
+const validChars = ['R', 'P', 'S'];
 
 while (carryOn === true) {
   /* Player 1 input section */
@@ -13,7 +14,7 @@ while (carryOn === true) {
 
   // Loops as long as a falsy value is inputted
   while (!input1) {
-    console.log('\nERROR: Player 1, you must enter a string!\n');
+    console.log(`\n${c.red('ERROR: Player 1, you must enter a string!')}\n`);
     input1 = prompt(
       'Player 1 => please select from rock, paper and scissors (R, P, S):  '
     );
@@ -22,7 +23,9 @@ while (carryOn === true) {
   // Loops as long as the inputted string is not from the valid values
   while (!validChars.includes(input1.toUpperCase())) {
     console.log(
-      '\nERROR: Player 1, the string you have entered is not valid!\n'
+      `\n${c.red(
+        'ERROR: Player 1, the string you have entered is not valid!'
+      )}\n`
     );
     input1 = prompt(
       'Player 1 => please select from rock, paper and scissors (R, P, S):  '
@@ -37,7 +40,7 @@ while (carryOn === true) {
 
   // Loops as long as a falsy value is inputted
   while (!input2) {
-    console.log('\nERROR: Player 2, you must enter a string!\n');
+    console.log(`\n${c.red('ERROR: Player 2, you must enter a string!')}\n`);
     input2 = prompt(
       'Player 2 => please select from rock, paper and scissors (R, P, S):  '
     );
@@ -46,7 +49,9 @@ while (carryOn === true) {
   // Loops as long as the inputted string is not from the valid values
   while (!validChars.includes(input2.toUpperCase())) {
     console.log(
-      '\nERROR: Player 2, the string you have entered is not valid!\n'
+      `\n${c.red(
+        'ERROR: Player 2, the string you have entered is not valid!'
+      )}\n`
     );
     input2 = prompt(
       'Player 2 => please select from rock, paper and scissors (R, P, S):  '
@@ -55,13 +60,19 @@ while (carryOn === true) {
 
   const result = rps(input1.toUpperCase(), input2.toUpperCase());
 
-  console.log(`\n${result}\n`);
+  result.includes('Player 1')
+    ? console.log(c.blueBright(`\n${result}\n`))
+    : result.includes('Player 2')
+    ? console.log(c.magenta(`\n${result}\n`))
+    : console.log(c.green(`\n${result}\n`));
+
+  /* Continuation prompt section */
 
   const bool = prompt('Do you want to play another round? (y/n)  ');
 
   if (bool.toLowerCase() === 'n') {
     carryOn = false;
-    console.log('\nHope you had fun! :)\n');
+    console.log(c.cyan('\nHope you had fun! :)\n'));
   } else {
     console.log('\n');
   }
